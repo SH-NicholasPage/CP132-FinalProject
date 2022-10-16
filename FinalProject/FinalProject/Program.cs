7e3d338290280b27b4f84e03be2e0f15f8c73dd1
SH-NicholasPage <npage@southhills.edu>
2022-10-16 16:32:33 -0400

    Initial commit

---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+using System;
+
+namespace FinalProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            //TODO: Start your code here
+        }
+    }
+}
